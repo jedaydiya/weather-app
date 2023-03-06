87591a5327a63c02e57660cd338e536fde242ea6
jedaydiya <joloanjed@gmail.com>
2023-03-06 10:28:46 +0800

    API search fixed

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,12 @@ import { useState, ChangeEvent } from "react";
 import "./App.css";
 
 function App() {
-  // const apiKey = process.env.REACT_APP_API_KEY;
   const [term, setTerm] = useState<string>("");
   const [options, setOptions] = useState<[]>([]);
-
+  const ApiKey = import.meta.env.VITE_APP_API_KEY;
   const getSearchOptions = (value: string) => {
     fetch(
-      `http://api.openweathermap.org/geo/1.0/direct?q=${value.trim()}&limit=5&appid=${
-        import.meta.env.REACT_APP_API_KEY
-      }`
+      `http://api.openweathermap.org/geo/1.0/direct?q=${value.trim()}&limit=5&appid=${ApiKey}`
     )
       .then((res) => res.json())
       .then((data) => setOptions(data));
@@ -32,19 +29,21 @@ function App() {
             Weather <span className="font-black">Report</span>
           </h1>
 
-          <div className="flex mt-5">
-            <input
-              value={term}
-              type="text"
-              className="px-2 py-1 border-2 border-white rounded-1-md"
-              onChange={onInputChange}
-            />
+          <div className="relative flex mt-10 md:mt-4">
+            <div className="flex mt-5">
+              <input
+                value={term}
+                type="text"
+                className="px-2 py-1 border-2 border-white rounded-1-md"
+                onChange={onInputChange}
+              />
 
-            <ul className="absolute top-0 bg-white ml-1 rounded-b-md">
-              {options.map((option: { name: string }) => (
-                <p>{option.name}</p>
-              ))}
-            </ul>
+              <ul className="absolute top-9 bg-white ml-1 rounded-b-md">
+                {options.map((option: { name: string }) => (
+                  <p>{option.name}</p>
+                ))}
+              </ul>
+            </div>
 
             <button className="px-2 py-2 border-2 cursor-pointer rounded-r-md border-zinc-100 hover:border-zinc-500 hover:text-zinc-500 text-zinc-1">
               Search
