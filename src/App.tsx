5f3fde0b95106d11480bdf8bcf001147ad784dd6
jedaydiya <joloanjed@gmail.com>
2023-03-06 19:05:54 +0800

    added useForecast hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,10 @@
-import { useState, ChangeEvent, useEffect } from "react";
-import { optionType } from "./types";
 import "./App.css";
 import Search from "./components/Search";
+import useForecast from "./hooks/useForecast";
 
 function App() {
-  const [term, setTerm] = useState<string>("");
-  const [options, setOptions] = useState<[]>([]);
-  const [city, setCity] = useState<optionType | null>(null);
-  const [forecast, setForecast] = useState<null>(null);
-  const ApiKey = import.meta.env.VITE_APP_API_KEY;
-  const getSearchOptions = (value: string) => {
-    fetch(
-      `http://api.openweathermap.org/geo/1.0/direct?q=${value.trim()}&limit=5&appid=${ApiKey}`
-    )
-      .then((res) => res.json())
-      .then((data) => setOptions(data));
-  };
-
-  const onInputChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const value = e.target.value.trim();
-    setTerm(value);
-
-    if (value === "") return;
-
-    getSearchOptions(value);
-  };
-
-  const getForecast = (city: optionType) => {
-    fetch(
-      `https://api.openweathermap.org/data/2.5/weather?lat=${city.lat}&lon=${city.lon}&units=metric&appid=${ApiKey}`
-    )
-      .then((res) => res.json())
-      .then((data) => setForecast(data));
-  };
-
-  const onSubmit = () => {
-    if (!city) return;
-
-    getForecast(city);
-  };
-  const onOptionSelect = (option: optionType) => {
-    setCity(option);
-  };
-
-  useEffect(() => {
-    if (city) {
-      setTerm(city.name);
-      setOptions([]);
-    }
-  }, [city]);
-
+  const { term, options, forecast, onInputChange, onOptionSelect, onSubmit } =
+    useForecast();
   return (
     <>
       <main className="flex justify-center items-center h-[100vh] w-full">
---
+++ src/hooks/useForecast.ts
@@ -0,0 +1,59 @@
+import { useState, ChangeEvent, useEffect } from "react";
+import { optionType } from "../types";
+const useForecast = () => {
+  const [term, setTerm] = useState<string>("");
+  const [options, setOptions] = useState<[]>([]);
+  const [city, setCity] = useState<optionType | null>(null);
+  const [forecast, setForecast] = useState<null>(null);
+  const ApiKey = import.meta.env.VITE_APP_API_KEY;
+  const getSearchOptions = (value: string) => {
+    fetch(
+      `http://api.openweathermap.org/geo/1.0/direct?q=${value.trim()}&limit=5&appid=${ApiKey}`
+    )
+      .then((res) => res.json())
+      .then((data) => setOptions(data));
+  };
+
+  const onInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value.trim();
+    setTerm(value);
+
+    if (value === "") return;
+
+    getSearchOptions(value);
+  };
+
+  const getForecast = (city: optionType) => {
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${city.lat}&lon=${city.lon}&units=metric&appid=${ApiKey}`
+    )
+      .then((res) => res.json())
+      .then((data) => setForecast(data));
+  };
+
+  const onSubmit = () => {
+    if (!city) return;
+
+    getForecast(city);
+  };
+  const onOptionSelect = (option: optionType) => {
+    setCity(option);
+  };
+
+  useEffect(() => {
+    if (city) {
+      setTerm(city.name);
+      setOptions([]);
+    }
+  }, [city]);
+
+  return {
+    term,
+    options,
+    forecast,
+    onInputChange,
+    onOptionSelect,
+    onSubmit,
+  };
+};
+export default useForecast;
